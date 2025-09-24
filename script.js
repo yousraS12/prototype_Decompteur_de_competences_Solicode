b711eee8202a3124a03a72b7724a6bbefbcf9a4f
réalisation <boujeab2020@gmail.com>
2025-09-24 16:56:34 +0100

    prototype

---
+++ script.js
@@ -0,0 +1,12 @@
+let boit=["C2","C3","C1","C2","C2","C3","C1"];
+let element ={}; 
+for(let i=0; i<boit.length;i++){
+    let A= boit[i];
+    if(element[A]){
+        element[A]++;
+    }else{
+        element[A]=1
+    }
+}
+console.log(boit.length);
+console.log(element);
